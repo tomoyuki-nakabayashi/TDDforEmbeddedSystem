e55bdedf0e2df2613f2f7f6307ab4540966b8bae
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-01-21 21:49:51 +0900

    Add SuccessCaptureEvent

--- test/evdev_sample_test.cpp
+++ test/evdev_sample_test.cpp
@@ -4,11 +4,13 @@
 // You might exucute these tests as root user.
 
 #include <gtest/gtest.h>
+#include <thread>
 extern "C" {
 #include <libevdev/libevdev.h>
+#include <linux/input.h>
+#include <sys/time.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <unistd.h>
 }
 
 namespace evdev_sample_test {
@@ -39,5 +41,32 @@ TEST_F(EvdevSampleTest, ReturnsEAGAIN) {
   struct input_event ev {};
   rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
   EXPECT_EQ(-EAGAIN, rc);
+}
+
+static void write_key_event(int code, int value, int fd)
+{
+  struct input_event key_event;
+  
+  gettimeofday(&key_event.time, NULL);
+  key_event.type = EV_KEY;
+  key_event.code = code;
+  key_event.value = value;
+  write(fd, &key_event, sizeof(key_event));
+}
+
+TEST_F(EvdevSampleTest, SuccessCaptureEvent) {
+  struct libevdev *dev = nullptr;
+  int fd = open("/dev/input/event2", O_RDONLY|O_NONBLOCK);
+  int rc = libevdev_new_from_fd(fd, &dev);
+
+  int ev_count = 0;
+  struct input_event ev {};
+  // lambda that's result will change only when capturing an event
+  std::thread t {([&]{ while(ev_count == 0) {ev_count += (libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev) != -EAGAIN); }})};
+  // write an event
+  write_key_event(KEY_A, 1, fd);
+  t.join();
+
+  EXPECT_EQ(1, ev_count);
 }
 }
