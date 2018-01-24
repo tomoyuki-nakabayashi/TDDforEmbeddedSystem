16e6033822b3f410778b3993c4504bf1cdddb1db
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-01-24 20:53:58 +0900

    Add HasEventPending... But test won't stop...

--- test/evdev_sample_test.cpp
+++ test/evdev_sample_test.cpp
@@ -5,6 +5,8 @@
 
 #include <gtest/gtest.h>
 #include <thread>
+#include <chrono>
+#include <future>
 extern "C" {
 #include <libevdev/libevdev.h>
 #include <linux/input.h>
@@ -14,38 +16,56 @@ extern "C" {
 }
 
 namespace evdev_sample_test {
-class EvdevSampleTest : public ::testing::Test {
-protected:
-  virtual void SetUp()
-  {
-  }
+class EvdevSampleOpenTest : public ::testing::Test {
+ protected:
+    virtual void SetUp()
+    {
+    }
 
-  virtual void TearDown()
-  {
-  }
+    virtual void TearDown()
+    {
+    }
 };
 
-TEST_F(EvdevSampleTest, InputEventFileCanOpenAsRoot) {
-  struct libevdev *dev = nullptr;
+TEST_F(EvdevSampleOpenTest, InputEventFileCanOpenAsRoot) {
+  struct libevdev *dev {nullptr};
   int fd = open("/dev/input/event2", O_RDONLY|O_NONBLOCK);
   int rc = libevdev_new_from_fd(fd, &dev);
 
   EXPECT_GE(rc, 0);
 }
 
-TEST_F(EvdevSampleTest, ReturnsEAGAIN) {
-  struct libevdev *dev = nullptr;
-  int fd = open("/dev/input/event2", O_RDONLY|O_NONBLOCK);
-  int rc = libevdev_new_from_fd(fd, &dev);
+class EvdevSampleTest : public ::testing::Test {
+ public:
+    EvdevSampleTest(): evdev_{nullptr}, fd_{} {}
 
+ protected:
+    virtual void SetUp()
+    {
+      fd_ = open("/dev/input/event2", O_RDONLY|O_NONBLOCK);
+      int rc = libevdev_new_from_fd(fd_, &evdev_);
+    }
+
+    virtual void TearDown()
+    {
+      // Flush all input events.
+      input_event ev {};
+      while(libevdev_next_event(evdev_, LIBEVDEV_READ_FLAG_NORMAL, &ev) != -EAGAIN) {}
+    }
+ protected:
+    libevdev *evdev_;
+    int fd_;
+};
+
+TEST_F(EvdevSampleTest, ReturnsEAGAIN) {
   struct input_event ev {};
-  rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
-  EXPECT_EQ(-EAGAIN, rc);
+  int actual = libevdev_next_event(evdev_, LIBEVDEV_READ_FLAG_NORMAL, &ev);
+  EXPECT_EQ(-EAGAIN, actual);
 }
 
 static void write_key_event(int code, int value, int fd)
 {
-  struct input_event key_event;
+  struct input_event key_event {};
   
   gettimeofday(&key_event.time, NULL);
   key_event.type = EV_KEY;
@@ -55,18 +75,25 @@ static void write_key_event(int code, int value, int fd)
 }
 
 TEST_F(EvdevSampleTest, SuccessCaptureEvent) {
-  struct libevdev *dev = nullptr;
-  int fd = open("/dev/input/event2", O_RDONLY|O_NONBLOCK);
-  int rc = libevdev_new_from_fd(fd, &dev);
-
   int ev_count = 0;
   struct input_event ev {};
   // lambda that's result will change only when capturing an event
-  std::thread t {([&]{ while(ev_count == 0) {ev_count += (libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev) != -EAGAIN); }})};
+  std::thread t {([&]{ while(ev_count == 0) {ev_count += (libevdev_next_event(evdev_, LIBEVDEV_READ_FLAG_NORMAL, &ev) != -EAGAIN); }})};
+
   // write an event
-  write_key_event(KEY_A, 1, fd);
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  write_key_event(KEY_A, 1, fd_);
   t.join();
 
   EXPECT_EQ(1, ev_count);
 }
+
+TEST_F(EvdevSampleTest, HasEventPending) {
+  auto event_pending = std::async(std::launch::async, [this]{ while(libevdev_has_event_pending(evdev_) == 0) {} return 1;});
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  write_key_event(KEY_A, 0, fd_);
+
+  EXPECT_EQ(1, event_pending.get());
 }
+}  // namespace evdev_sample_test
