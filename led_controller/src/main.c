b1634fae5233fcc18cdd68fb4f315d3156c47aad
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-03-19 14:40:24 +0900

    refactor: main

--- led_controller/src/main.c
+++ led_controller/src/main.c
@@ -7,21 +7,22 @@
 #include <detector/key_input_detector.h>
 #include <detector/event_detector.h>
 #include <detector/timeout_detector.h>
+#include <operator/operator.h>
+#include <operator/led_operator_factory.h>
 #include <drivers/led_driver.h>
 #include <utils/logger.h>
 
 #define KEYBOARD_DEVICE "/dev/input/event2"
 #define LED_DEVICE      "/sys/class/leds/input2::capslock/brightness"
-#define NUM_DETECTORS 2
+#define NUM_OPERATION_ON_DETECTION 2
 
 int main(void) {
   struct timeval kTime = {};
   const struct input_event kPressA = {kTime, EV_KEY, KEY_A, INPUT_KEY_PRESSED};
 
-  EventDetector detectors[NUM_DETECTORS+1];  // To null-terminate
+  EventDetector detectors[NUM_OPERATION_ON_DETECTION];
   detectors[0] = CreateKeyInputDetector(KEYBOARD_DEVICE, &kPressA);
   detectors[1] = CreateTimeOutDetector(5000, TIMER_ONE_SHOT);
-  detectors[2] = NULL;  // null-terminate
   
   LedDriver caps_led = CreateLedDriver();
   if (InitLedDriver(caps_led, LED_DEVICE) != LED_DRIVER_SUCCESS) {
@@ -29,18 +30,23 @@ int main(void) {
     exit(1);
   }
 
-  for(int i = 0; detectors[i] != NULL; i++) {
+  Operator operators[NUM_OPERATION_ON_DETECTION];
+  operators[0] = LedOperatorFactory(caps_led, OP_LED_TURN_ON);
+  operators[1] = LedOperatorFactory(caps_led, OP_LED_TURN_OFF);
+
+  for(int i = 0; i < NUM_OPERATION_ON_DETECTION; i++) {
     StartEventDetector(detectors[i]);
     while(CheckEvent(detectors[i]) != DETECTOR_EVENT_DETECTED) {}
 
-    ToggleLed(caps_led);
+    TriggerOperation(operators[i]);
   }
 
-  for(int i = 0; detectors[i] != NULL; i++) {
+  for(int i = 0; i < NUM_OPERATION_ON_DETECTION; i++) {
     CleanupEventDetector(detectors[i]);
-    DestroyKeyInputDetector(detectors[i]);
   }
 
+  DestroyKeyInputDetector(detectors[0]);
+  DestroyTimeOutDetector(detectors[1]);
   CleanupLedDriver(caps_led);
   DestroyLedDriver(caps_led);
 
