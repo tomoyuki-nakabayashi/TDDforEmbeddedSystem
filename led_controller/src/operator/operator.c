64a0fbf47b559a70bfa39cbfe6902e357167ec10
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-03-22 20:53:51 +0900

    refactor: Remove operator.

--- led_controller/src/operator/operator.c
+++
@@ -1,8 +0,0 @@
-// Copyright <2018> <Tomoyuki Nakabayashi>
-// This software is released under the MIT License, see LICENSE.
-
-#include <operator/operator.h>
-
-void TriggerOperation(Operator op) {
-  op->vtable->Operate(op);
-}
